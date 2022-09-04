337368ea4011e542bcbd8dd5e13f747c1c8b9f13
valflrt <valflrt@users.noreply.github.com>
2022-09-04 18:19:36 +0000

    deploy: 13f9d37af48861d74f573169f13edb303bdaf800

---
+++ assets/CopyLink.6ab50cf5.js
@@ -0,0 +1,2 @@
+import{a as r,B as a,E as s}from"./index.983b2ba4.js";const x=o=>{let{textToCopy:p,notificationTimeout:t,...e}=o;return r(a,{toDo:()=>{navigator.clipboard.writeText(o.textToCopy).then(()=>{s("Copied !",{duration:t!=null?t:2e3})})},...e})};export{x as C};
+//# sourceMappingURL=CopyLink.6ab50cf5.js.map
---
+++ assets/NotFound.b7746222.js
@@ -0,0 +1,2 @@
+import{j as n,a as e,P as o}from"./index.983b2ba4.js";const a=()=>(document.title="404 \u2013 valflrt.dev",n("div",{className:"main notFound",children:[e("h1",{className:"fortyHundredAndFour",children:"404"}),e("p",{children:"There's nothing here !"}),e(o,{to:"/",className:()=>"link underlined",children:"Back home"})]}));export{a as default};
+//# sourceMappingURL=NotFound.b7746222.js.map
