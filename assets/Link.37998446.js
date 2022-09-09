409360ce2930c9f527b556c63ea5372a6ec0ba41
valflrt <valflrt@users.noreply.github.com>
2022-09-09 16:52:19 +0000

    deploy: 01b1f39ac6622295fd51f96fc1c219ea70046804

--- assets/Link.37998446.js
+++
@@ -1,2 +0,0 @@
-import{a as o}from"./index.dc652c78.js";const a=r=>{let{to:t,...e}=r;return o("a",{href:t,...e})};export{a as L};
-//# sourceMappingURL=Link.37998446.js.map
---
+++ assets/Link.570fc50b.js
@@ -0,0 +1,2 @@
+import{a as o}from"./index.d9b17ce7.js";const a=r=>{let{to:t,...e}=r;return o("a",{href:t,...e})};export{a as L};
+//# sourceMappingURL=Link.570fc50b.js.map
