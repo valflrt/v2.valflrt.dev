dbc280e76aaedca38a08854d4f2ff31b42478763
valflrt <valflrt@users.noreply.github.com>
2022-09-14 15:42:11 +0000

    deploy: 595342686f6fe6953e40fb80eeb0697fd89ee3a4

--- assets/NotFound.192ef126.js
+++
@@ -1,2 +0,0 @@
-import{j as n,a as e,P as t}from"./index.a536e81a.js";function o(){return document.title="404 \u2013 valflrt.dev",n("div",{className:"main notFound",children:[e("h1",{className:"mainTitle fortyHundredAndFour",children:"404"}),e("p",{children:"There's nothing here !"}),e(t,{to:"/",className:"link underlined",children:"Back home"})]})}export{o as default};
-//# sourceMappingURL=NotFound.192ef126.js.map
