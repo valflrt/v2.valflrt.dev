9291bd6814d05314d043fa42bd73d724077f7b54
valflrt <valflrt@pm.me>
2025-04-19 18:33:13 +0200

    Improve html and icon related code in index.js

--- public/index.js
+++ public/index.js
@@ -205,27 +205,29 @@ function createRouter(routes, callback) {
  * Creates a DOM element.
  * @template {keyof HTMLElementTagNameMap} K
  * @param {K} tag
- * @param {TagPropsMap[K]} props
- * @param  {...NodeOrFalsy} children
+ * @param {TagPropsMap[K]} [props]
+ * @param  {...NodeOrFalsy} [children]
  * @returns {HTMLElementTagNameMap[K]}
  */
 function createElement(tag, props, ...children) {
   let element = document.createElement(tag);
 
-  Object.entries(props).forEach((pair) => {
-    let [k, v] = pair;
-    element.setAttribute(k, v);
-  });
-
-  children
-    .filter((v) => !!v)
-    .forEach((child) => {
-      if (typeof child === "string") {
-        element.appendChild(document.createTextNode(child));
-      } else {
-        element.appendChild(child);
-      }
+  if (!!props)
+    Object.entries(props).forEach((pair) => {
+      let [k, v] = pair;
+      element.setAttribute(k, v);
     });
+
+  if (!!children)
+    children
+      .filter((v) => !!v)
+      .forEach((child) => {
+        if (typeof child === "string") {
+          element.appendChild(document.createTextNode(child));
+        } else {
+          element.appendChild(child);
+        }
+      });
 
   return element;
 }
@@ -317,25 +319,11 @@ const img = (props, ...children) => createElement("img", props, ...children);
  * Creates a br element.
  * @returns {HTMLBRElement}
  */
-const br = () => createElement("br", {});
+const br = () => createElement("br");
 
 // ----- icons ---------------
 
 // icons from https://simpleicons.org and https://feathericons.com/
-
-/**
- * Parse a svg string to a HTMLElement.
- * @param {string} svgString
- * @returns {HTMLElement}
- */
-function parseSvg(svgString) {
-  let parser = new DOMParser();
-  return parser.parseFromString(svgString, "image/svg+xml").documentElement;
-}
-
-/**
- * @type {{ [key: string]: string; }}
- */
 const icons = {
   atSign:
     '<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-at-sign"><circle cx="12" cy="12" r="4"></circle><path d="M16 8v5a3 3 0 0 0 6 0v-1a10 10 0 1 0-3.92 7.94"></path></svg>',
@@ -362,6 +350,15 @@ const icons = {
   rust: '<svg role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><title>Rust</title><path d="M23.8346 11.7033l-1.0073-.6236a13.7268 13.7268 0 00-.0283-.2936l.8656-.8069a.3483.3483 0 00-.1154-.578l-1.1066-.414a8.4958 8.4958 0 00-.087-.2856l.6904-.9587a.3462.3462 0 00-.2257-.5446l-1.1663-.1894a9.3574 9.3574 0 00-.1407-.2622l.49-1.0761a.3437.3437 0 00-.0274-.3361.3486.3486 0 00-.3006-.154l-1.1845.0416a6.7444 6.7444 0 00-.1873-.2268l.2723-1.153a.3472.3472 0 00-.417-.4172l-1.1532.2724a14.0183 14.0183 0 00-.2278-.1873l.0415-1.1845a.3442.3442 0 00-.49-.328l-1.076.491c-.0872-.0476-.1742-.0952-.2623-.1407l-.1903-1.1673A.3483.3483 0 0016.256.955l-.9597.6905a8.4867 8.4867 0 00-.2855-.086l-.414-1.1066a.3483.3483 0 00-.5781-.1154l-.8069.8666a9.2936 9.2936 0 00-.2936-.0284L12.2946.1683a.3462.3462 0 00-.5892 0l-.6236 1.0073a13.7383 13.7383 0 00-.2936.0284L9.9803.3374a.3462.3462 0 00-.578.1154l-.4141 1.1065c-.0962.0274-.1903.0567-.2855.086L7.744.955a.3483.3483 0 00-.5447.2258L7.009 2.348a9.3574 9.3574 0 00-.2622.1407l-1.0762-.491a.3462.3462 0 00-.49.328l.0416 1.1845a7.9826 7.9826 0 00-.2278.1873L3.8413 3.425a.3472.3472 0 00-.4171.4171l.2713 1.1531c-.0628.075-.1255.1509-.1863.2268l-1.1845-.0415a.3462.3462 0 00-.328.49l.491 1.0761a9.167 9.167 0 00-.1407.2622l-1.1662.1894a.3483.3483 0 00-.2258.5446l.6904.9587a13.303 13.303 0 00-.087.2855l-1.1065.414a.3483.3483 0 00-.1155.5781l.8656.807a9.2936 9.2936 0 00-.0283.2935l-1.0073.6236a.3442.3442 0 000 .5892l1.0073.6236c.008.0982.0182.1964.0283.2936l-.8656.8079a.3462.3462 0 00.1155.578l1.1065.4141c.0273.0962.0567.1914.087.2855l-.6904.9587a.3452.3452 0 00.2268.5447l1.1662.1893c.0456.088.0922.1751.1408.2622l-.491 1.0762a.3462.3462 0 00.328.49l1.1834-.0415c.0618.0769.1235.1528.1873.2277l-.2713 1.1541a.3462.3462 0 00.4171.4161l1.153-.2713c.075.0638.151.1255.2279.1863l-.0415 1.1845a.3442.3442 0 00.49.327l1.0761-.49c.087.0486.1741.0951.2622.1407l.1903 1.1662a.3483.3483 0 00.5447.2268l.9587-.6904a9.299 9.299 0 00.2855.087l.414 1.1066a.3452.3452 0 00.5781.1154l.8079-.8656c.0972.0111.1954.0203.2936.0294l.6236 1.0073a.3472.3472 0 00.5892 0l.6236-1.0073c.0982-.0091.1964-.0183.2936-.0294l.8069.8656a.3483.3483 0 00.578-.1154l.4141-1.1066a8.4626 8.4626 0 00.2855-.087l.9587.6904a.3452.3452 0 00.5447-.2268l.1903-1.1662c.088-.0456.1751-.0931.2622-.1407l1.0762.49a.3472.3472 0 00.49-.327l-.0415-1.1845a6.7267 6.7267 0 00.2267-.1863l1.1531.2713a.3472.3472 0 00.4171-.416l-.2713-1.1542c.0628-.0749.1255-.1508.1863-.2278l1.1845.0415a.3442.3442 0 00.328-.49l-.49-1.076c.0475-.0872.0951-.1742.1407-.2623l1.1662-.1893a.3483.3483 0 00.2258-.5447l-.6904-.9587.087-.2855 1.1066-.414a.3462.3462 0 00.1154-.5781l-.8656-.8079c.0101-.0972.0202-.1954.0283-.2936l1.0073-.6236a.3442.3442 0 000-.5892zm-6.7413 8.3551a.7138.7138 0 01.2986-1.396.714.714 0 11-.2997 1.396zm-.3422-2.3142a.649.649 0 00-.7715.5l-.3573 1.6685c-1.1035.501-2.3285.7795-3.6193.7795a8.7368 8.7368 0 01-3.6951-.814l-.3574-1.6684a.648.648 0 00-.7714-.499l-1.473.3158a8.7216 8.7216 0 01-.7613-.898h7.1676c.081 0 .1356-.0141.1356-.088v-2.536c0-.074-.0536-.0881-.1356-.0881h-2.0966v-1.6077h2.2677c.2065 0 1.1065.0587 1.394 1.2088.0901.3533.2875 1.5044.4232 1.8729.1346.413.6833 1.2381 1.2685 1.2381h3.5716a.7492.7492 0 00.1296-.0131 8.7874 8.7874 0 01-.8119.9526zM6.8369 20.024a.714.714 0 11-.2997-1.396.714.714 0 01.2997 1.396zM4.1177 8.9972a.7137.7137 0 11-1.304.5791.7137.7137 0 011.304-.579zm-.8352 1.9813l1.5347-.6824a.65.65 0 00.33-.8585l-.3158-.7147h1.2432v5.6025H3.5669a8.7753 8.7753 0 01-.2834-3.348zm6.7343-.5437V8.7836h2.9601c.153 0 1.0792.1772 1.0792.8697 0 .575-.7107.7815-1.2948.7815zm10.7574 1.4862c0 .2187-.008.4363-.0243.651h-.9c-.09 0-.1265.0586-.1265.1477v.413c0 .973-.5487 1.1846-1.0296 1.2382-.4576.0517-.9648-.1913-1.0275-.4717-.2704-1.5186-.7198-1.8436-1.4305-2.4034.8817-.5599 1.799-1.386 1.799-2.4915 0-1.1936-.819-1.9458-1.3769-2.3153-.7825-.5163-1.6491-.6195-1.883-.6195H5.4682a8.7651 8.7651 0 014.907-2.7699l1.0974 1.151a.648.648 0 00.9182.0213l1.227-1.1743a8.7753 8.7753 0 016.0044 4.2762l-.8403 1.8982a.652.652 0 00.33.8585l1.6178.7188c.0283.2875.0425.577.0425.8717zm-9.3006-9.5993a.7128.7128 0 11.984 1.0316.7137.7137 0 01-.984-1.0316zm8.3389 6.71a.7107.7107 0 01.9395-.3625.7137.7137 0 11-.9405.3635z"/></svg>',
   user: '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-user"><path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2"></path><circle cx="12" cy="7" r="4"></circle></svg>',
 };
+
+/**
+ * Returns the corresponding icon element.
+ * @param {keyof typeof icons} name
+ */
+function icon(name) {
+  let parser = new DOMParser();
+  return parser.parseFromString(icons[name], "image/svg+xml").documentElement;
+}
 
 // ----- routes ---------------
 
@@ -391,10 +388,7 @@ const routes = [
             { class: "header" },
             div({ class: "title code" }, `valflrt/${project.id}`),
             project.used &&
-              div(
-                { class: "icons" },
-                ...project.used.map((v) => parseSvg(icons[v])),
-              ),
+              div({ class: "icons" }, ...project.used.map((v) => icon(v))),
           ),
           div(
             { class: "description" },
@@ -408,13 +402,13 @@ const routes = [
               a(
                 { href: l.url, class: "button clickable", target: "_blank" },
                 l.name,
-                parseSvg(l.icon ? icons[l.icon] : icons.externalLink),
+                icon(l.icon ?? icons.externalLink),
               ),
             ),
             a(
               { href: "#/projects", class: "button clickable" },
               "Back to projects",
-              parseSvg(icons.list),
+              icon("list"),
             ),
           ),
       );
@@ -438,10 +432,7 @@ const routes = [
                 { class: "header" },
                 div({ class: "title code" }, `valflrt/${p.id}`),
                 p.used &&
-                  div(
-                    { class: "icons" },
-                    ...p.used.map((v) => parseSvg(icons[v])),
-                  ),
+                  div({ class: "icons" }, ...p.used.map((v) => icon(v))),
               ),
               div(
                 { class: "description" },
@@ -457,7 +448,7 @@ const routes = [
             class: "button clickable",
           },
           "See more on github",
-          parseSvg(icons.github),
+          icon("github"),
         ),
       ),
   },
@@ -517,7 +508,7 @@ const routes = [
               class: "button clickable",
             },
             "Projects",
-            parseSvg(icons.list),
+            icon("list"),
           ),
           a(
             {
@@ -525,7 +516,7 @@ const routes = [
               class: "button clickable",
             },
             "Social",
-            parseSvg(icons.user),
+            icon("user"),
           ),
         ),
       ),
@@ -538,11 +529,8 @@ const routes = [
     content: () =>
       unit(
         h1({ class: "title" }, "Music"),
-
         p({}, "Oh... So you found my secret music display..."),
-
         p({}, "Those are some songs I really like:"),
-
         div(
           { class: "songs" },
           ...shuffle(songs)
@@ -564,9 +552,7 @@ const routes = [
               ),
             ),
         ),
-
         p({}, "Here are some of my favorite artists:"),
-
         div(
           { class: "artists" },
           ...shuffle(artists)
@@ -588,7 +574,6 @@ const routes = [
               ),
             ),
         ),
-
         p({ class: "faded" }, "randomized on page refresh"),
       ),
   },
@@ -610,7 +595,7 @@ const routes = [
               class: "button clickable",
             },
             "Github",
-            parseSvg(icons.github),
+            icon("github"),
           ),
           a(
             {
@@ -619,7 +604,7 @@ const routes = [
               class: "button clickable",
             },
             "Instagram",
-            parseSvg(icons.instagram),
+            icon("instagram"),
           ),
           button(
             {
@@ -627,7 +612,7 @@ const routes = [
               class: "button clickable copy",
             },
             "Discord",
-            parseSvg(icons.discord),
+            icon("discord"),
           ),
           // a(
           //   {
@@ -636,7 +621,7 @@ const routes = [
           //     class: "button clickable",
           //   },
           //   "Deviant Art",
-          //   parseSvg(icons.deviantart),
+          //   icon("deviantart"),
           // ),
           a(
             {
@@ -645,7 +630,7 @@ const routes = [
               class: "button clickable",
             },
             "Bluesky",
-            parseSvg(icons.bluesky),
+            icon("bluesky"),
           ),
           button(
             {
@@ -653,7 +638,7 @@ const routes = [
               class: "button clickable copy",
             },
             "Email",
-            parseSvg(icons.atSign),
+            icon("atSign"),
           ),
         ),
       ),
